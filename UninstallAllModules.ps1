9fd004171f8675531bf77bb24a4990134e0011e8
Katalin Hopkins <62229442+kahopkin@users.noreply.github.com>
2022-07-18 10:50:38 -0400

    Create UninstallAllModules.ps1

---
+++ UninstallAllModules.ps1
@@ -0,0 +1,59 @@
+﻿
+#UninstallAllModules
+<#
+    ## – Example using -WhatIf parameter:
+
+Uninstall-AllModules -TargetModule AzureAD -Force
+#>
+
+Function Uninstall-AllModules
+{
+    [CmdletBinding(SupportsShouldProcess)] 
+    param (
+             [Parameter(Mandatory = $true)][string] $TargetModule
+            ,[Parameter(Mandatory = $false)][string] $Version
+            ,[switch] $Force
+)
+
+    $AllModules = @()
+
+    Write-Host -ForegroundColor Yellow "Creating list of dependencies…"
+    $target = Find-Module $TargetModule
+    $target.Dependencies | ForEach-Object 
+    {
+        $AllModules += New-Object -TypeName psobject -Property @{ name = $_.name}
+       # Write-Host -ForegroundColor Cyan $_.name
+    }
+    $AllModules += New-Object -TypeName psobject -Property @{ name = $TargetModule}
+
+    $cnt = 1;
+    foreach ($module in $AllModules)
+    {
+        Write-Host (“[$cnt] – ” + ‘Uninstalling {0} version’ -f $module.name);
+        $cnt++;
+        try
+        {
+            if ($PSCmdlet.ShouldProcess($module.name, ‘Uninstall’))
+            {
+                Uninstall-Module -Name $module.name -Force:$Force -ErrorAction Stop;
+            };
+        }
+        catch
+        {
+            Write-Host (“`t” + $_.Exception.Message)
+        }
+    }
+}
+
+$targetModule = "PackageManagement"
+$version ="1.4.7"
+Uninstall-AllModules -TargetModule $targetModule -Force
+
+
+
+
+
+$target.Dependencies | ForEach-Object 
+{     
+    Write-Host -ForegroundColor Cyan 
+}
