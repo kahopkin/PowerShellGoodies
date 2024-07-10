ea41b7ff5a367a2ec83c3c94ec8ab8cc13ad5d9d
kahopkin <katalin.hopkins@microsoft.com>
2024-07-10 09:33:04 -0400

    put doc notes to front

--- MoveFiles/4_RobocopyMoveFiles.ps1
+++ MoveFiles/4_RobocopyMoveFiles.ps1
@@ -1,64 +1,7 @@
 ﻿<#
 C:\GitHub\PowerShellGoodies\MoveFiles\4_RobocopyMoveFiles.ps1
 #>
-
-
-
-Function global:RobocopyMoveFiles
-{
-	Param(
-		 [Parameter(Mandatory = $true)] [String]$Source
-		,[Parameter(Mandatory = $true)] [String]$Destination
-		
-	)
-
-	$today = Get-Date -Format 'MM-dd-yyyy HH:mm:ss'
-	Write-Host -ForegroundColor Magenta -BackgroundColor Black "`n *************[$today] START 4_RobocopyMoveFilesv *****************"
-	<#Write-Host -ForegroundColor White -BackgroundColor Black "Source= " $Source 	
-	#Write-Host -ForegroundColor Magenta -BackgroundColor Black 
-	Write-Host -ForegroundColor Magenta -BackgroundColor Black "to $Destination *****************"
-	#>
-
-	Write-Host -ForegroundColor Yellow "`$Source=" -NoNewline
-	Write-Host -ForegroundColor Cyan "`"$Source`""
-	#get # of folders and files:
-	$FolderCount = (Get-ChildItem -Path $Source -Recurse -Directory | Measure-Object).Count
-	$FileCount = (Get-ChildItem -Path $Source -Recurse -File | Measure-Object).Count
-	
-	Write-Host -ForegroundColor Yellow "`$FolderCount= "  -NoNewline
-	Write-Host -ForegroundColor Cyan "`"$FolderCount`""
-
-	Write-Host -ForegroundColor Yellow "`$FileCount= "  -NoNewline
-	Write-Host -ForegroundColor Cyan "`"$FileCount`""
-
-	Write-Host -ForegroundColor Yellow "`$Destination=" -NoNewline
-	Write-Host -ForegroundColor Cyan "`"$Destination`""	
- 
-	$TodayFolder  = (Get-Date -Format 'MM-dd-yyyy-HH-mm-ss')
-	$SourceFolder = Get-Item -Path $Source
-	$LogFile = $TodayFolderPath = $Destination + "\" + $TodayFolder + "_" + $SourceFolder.Name + ".log"
-
-
-	$SourceFileNameArr = $Source.split("\")
-	$SourceFileName = $SourceFileNameArr[$SourceFileNameArr.Count-1]
-	$DestinationFolder = $Destination + "\" + $SourceFileName
-
-	If( (Test-Path $DestinationFolder) -eq $false)
-	{
-		#$DestinationFolder = (New-Item -Path $Destination -Name $SourceFileName -ItemType Directory).FullName
-		$DestinationFolder = (New-Item -Path $Destination -Name $SourceFileName -ItemType Directory)
-		#$Destination = New-Item -Path $Destination -Name $SourceFileName -ItemType Directory
-		$Destination = $DestinationPath = $DestinationFolder.FullName
-		Write-Host -ForegroundColor Green "CREATED DESTINATION FOLDER:"
-		Write-Host -ForegroundColor White "`$DestinationFolder=" -NoNewline
-		Write-Host -ForegroundColor Yellow "`"$DestinationFolder`""
-
-		Write-Host -ForegroundColor Cyan "`$DestinationPath=" -NoNewline
-		Write-Host -ForegroundColor Yellow "`"$DestinationPath`""
-	}
-
-
-	 <# 
+<# 
 		#https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
 		ROBOCOPY OPTIONS and SWITCHES:
 		*** Copy options: ****
@@ -251,6 +194,63 @@ Function global:RobocopyMoveFiles
 		If /LFSM is specified with no explicit floor value, the floor is set to 10% of the destination volume's size. 
 		Low free space mode is incompatible with /MT and /EFSRAW.
 	 #>
+
+
+Function global:RobocopyMoveFiles
+{
+	Param(
+		 [Parameter(Mandatory = $true)] [String]$Source
+		,[Parameter(Mandatory = $true)] [String]$Destination
+		
+	)
+
+	$today = Get-Date -Format 'MM-dd-yyyy HH:mm:ss'
+	Write-Host -ForegroundColor Magenta -BackgroundColor Black "`n *************[$today] START 4_RobocopyMoveFilesv *****************"
+	<#Write-Host -ForegroundColor White -BackgroundColor Black "Source= " $Source 	
+	#Write-Host -ForegroundColor Magenta -BackgroundColor Black 
+	Write-Host -ForegroundColor Magenta -BackgroundColor Black "to $Destination *****************"
+	#>
+
+	Write-Host -ForegroundColor Yellow "`$Source=" -NoNewline
+	Write-Host -ForegroundColor Cyan "`"$Source`""
+	#get # of folders and files:
+	$FolderCount = (Get-ChildItem -Path $Source -Recurse -Directory | Measure-Object).Count
+	$FileCount = (Get-ChildItem -Path $Source -Recurse -File | Measure-Object).Count
+	
+	Write-Host -ForegroundColor Yellow "`$FolderCount= "  -NoNewline
+	Write-Host -ForegroundColor Cyan "`"$FolderCount`""
+
+	Write-Host -ForegroundColor Yellow "`$FileCount= "  -NoNewline
+	Write-Host -ForegroundColor Cyan "`"$FileCount`""
+
+	Write-Host -ForegroundColor Yellow "`$Destination=" -NoNewline
+	Write-Host -ForegroundColor Cyan "`"$Destination`""	
+ 
+	$TodayFolder  = (Get-Date -Format 'MM-dd-yyyy-HH-mm-ss')
+	$SourceFolder = Get-Item -Path $Source
+	$LogFile = $TodayFolderPath = $Destination + "\" + $TodayFolder + "_" + $SourceFolder.Name + ".log"
+
+
+	$SourceFileNameArr = $Source.split("\")
+	$SourceFileName = $SourceFileNameArr[$SourceFileNameArr.Count-1]
+	$DestinationFolder = $Destination + "\" + $SourceFileName
+
+	If( (Test-Path $DestinationFolder) -eq $false)
+	{
+		#$DestinationFolder = (New-Item -Path $Destination -Name $SourceFileName -ItemType Directory).FullName
+		$DestinationFolder = (New-Item -Path $Destination -Name $SourceFileName -ItemType Directory)
+		#$Destination = New-Item -Path $Destination -Name $SourceFileName -ItemType Directory
+		$Destination = $DestinationPath = $DestinationFolder.FullName
+		Write-Host -ForegroundColor Green "CREATED DESTINATION FOLDER:"
+		Write-Host -ForegroundColor White "`$DestinationFolder=" -NoNewline
+		Write-Host -ForegroundColor Yellow "`"$DestinationFolder`""
+
+		Write-Host -ForegroundColor Cyan "`$DestinationPath=" -NoNewline
+		Write-Host -ForegroundColor Yellow "`"$DestinationPath`""
+	}
+
+
+	 
 	 
 	<# To move all files and folders, including empty ones, with all attributes. 
 	 #Note that the source folder will also be deleted.
